3ab1736151290be7d9c4eef50b1d1344c38ad33d
Romain Mormont <romainmormont@hotmail.com>
2017-06-29 09:08:22 +0200

    add provider table creation

--- src/main/java/be/mormont/iacf/boncom/Main.java
+++ src/main/java/be/mormont/iacf/boncom/Main.java
@@ -1,11 +1,19 @@
 package be.mormont.iacf.boncom;
 
+import be.mormont.iacf.boncom.db.Database;
+
+import java.sql.SQLException;
+
 /**
  * Created by Romain on 28-06-17.
  * This is a class.
  */
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            Database.createDatabaseIfNotExist();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/be/mormont/iacf/boncom/db/Database.java
+++ src/main/java/be/mormont/iacf/boncom/db/Database.java
@@ -11,20 +11,41 @@ import java.sql.Statement;
  */
 public class Database {
     // JDBC driver name and database URL
-    static final String JDBC_DRIVER = "org.sqlite.JDBC";
-    static final String DB_URL = "jdbc:sqlite:database.db";
-    static final String DB_NAME
+    private static final String JDBC_DRIVER = "org.sqlite.JDBC";
+    private static final String DB_URL = "jdbc:sqlite:database.db";
+    private static final String DB_NAME = "iacf-orderform";
+
+    private static final String DB_TABLE_PROVIDER = "provider";
 
     //  Database credentials
-    static final String USER = "access";
-    static final String PASS = "";
+    private static final String USER = "access";
+    private static final String PASS = "";
 
     public static void createDatabaseIfNotExist() throws SQLException {
-        Connection c = DriverManager.getConnection(DB_URL);
-        c.setAutoCommit(false);
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
-        Statement statement = c.createStatement();
-        statement.executeUpdate("CREATE DATABASE " + DB_NAME);
+        try (Connection c = DriverManager.getConnection(DB_URL); Statement statement = c.createStatement()) {
+            c.setAutoCommit(false);
+
+            statement.executeUpdate("CREATE DATABASE " + DB_NAME);
+
+            // create provider table
+            statement.executeUpdate(
+                    "CREATE TABLE " + DB_TABLE_PROVIDER + " IF NOT EXISTS " +
+                            "(id INTEGER PRIMARY KEY, " +
+                            " name VARCHAR(255)," +
+                            " phoneNumbers VARCHAR(255)," +
+                            "street VARCHAR(511)," +
+                            "house_number VARCHAR(255)," +
+                            "box VARCHAR (255)," +
+                            "post_code VARCHAR(255)," +
+                            "city VARCHAR(255))");
+        }
+
 
     }
 }
