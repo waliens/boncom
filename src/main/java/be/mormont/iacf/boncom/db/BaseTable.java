fa641ce37a13093a132e842b228a3d41573f2e9d
Romain Mormont <romainmormont@hotmail.com>
2017-07-01 23:46:30 +0200

    change accessibility + implement getEntity

--- src/main/java/be/mormont/iacf/boncom/db/BaseTable.java
+++ src/main/java/be/mormont/iacf/boncom/db/BaseTable.java
@@ -8,7 +8,9 @@ import java.sql.SQLException;
  * Date: 01-07-17
  * By  : Mormont Romain
  */
-public interface BaseTable<T> {
-    String insertQuery();
-    PreparedStatement insertStatement(Connection conn, T object) throws SQLException;
+abstract class BaseTable<T> {
+    abstract String insertQuery();
+    abstract PreparedStatement insertStatement(Connection conn, T object) throws SQLException;
+    abstract String selectQuery();
+    abstract PreparedStatement selectStatement(Connection conn, long id) throws SQLException;
 }
--- src/main/java/be/mormont/iacf/boncom/db/EntityTable.java
+++ src/main/java/be/mormont/iacf/boncom/db/EntityTable.java
@@ -1,17 +1,19 @@
 package be.mormont.iacf.boncom.db;
 
+import be.mormont.iacf.boncom.data.Address;
 import be.mormont.iacf.boncom.data.Entity;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.ArrayList;
 
 /**
  * Date: 01-07-17
  * By  : Mormont Romain
  */
-public class EntityTable implements BaseTable<Entity> {
+public class EntityTable extends BaseTable<Entity> {
     static String FIELD_ID = "id";
     static String FIELD_ENTITY_NAME = "entity_name";
     static String FIELD_PHONE_NUMBERS = "phone_numbers";
@@ -24,7 +26,7 @@ public class EntityTable implements BaseTable<Entity> {
     static String NAME = "entity";
 
     @Override
-    public String insertQuery() {
+    String insertQuery() {
         return "INSERT INTO " + NAME + "(" +
                  EntityTable.FIELD_ENTITY_NAME + ", " + EntityTable.FIELD_PHONE_NUMBERS + ", " +
                  EntityTable.FIELD_STREET + ", " + EntityTable.FIELD_HOUSE_NUMBER + ", " +
@@ -34,7 +36,7 @@ public class EntityTable implements BaseTable<Entity> {
     }
 
     @Override
-    public PreparedStatement insertStatement(Connection conn, Entity object) throws SQLException {
+    PreparedStatement insertStatement(Connection conn, Entity object) throws SQLException {
         PreparedStatement statement = conn.prepareStatement(insertQuery());
         statement.setString(1, object.getName());
         statement.setString(2, String.join(",", object.getPhoneNumbers()));
@@ -44,5 +46,51 @@ public class EntityTable implements BaseTable<Entity> {
         statement.setString(6, object.getAddress().getPostCode());
         statement.setString(7, object.getAddress().getCity());
         return statement;
+    }
+
+    @Override
+    String selectQuery() {
+        return  "SELECT * FROM " + NAME + " WHERE " + FIELD_ID + "=?";
+    }
+
+    @Override
+    PreparedStatement selectStatement(Connection conn, long id) throws SQLException {
+        PreparedStatement statement = conn.prepareStatement(selectQuery());
+        statement.setLong(1, id);
+        return statement;
+    }
+
+    public Entity getEntity(long id) throws SQLException {
+        return Database.getDatabase().executePreparedStatement(new Database.PreparedStatementBuilder<Entity>() {
+            @Override
+            public PreparedStatement getStatement(Connection conn) throws SQLException {
+                return selectStatement(conn, id);
+            }
+
+            @Override
+            public Entity success(ResultSet resultSet) {
+                try {
+                    if (resultSet.next()) {
+                        Address entityAddress = new Address(
+                            resultSet.getString(3), // Street
+                            resultSet.getString(4), // Number
+                            resultSet.getString(5), // Box
+                            resultSet.getString(6), // PostCode
+                            resultSet.getString(7)  // City
+                        );
+
+                        return new Entity(
+                            resultSet.getString(1),
+                            entityAddress,
+                            resultSet.getString(8).split(",")
+                        );
+                    }
+                } catch (SQLException ignored) { }
+                return null;
+            }
+
+            @Override
+            public void failure(Exception e) { }
+        });
     }
 }
