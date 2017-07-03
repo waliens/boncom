3ee56bd531ebeacecfade4bbcc0c1fd41ede0eb4
Romain Mormont <romainmormont@hotmail.com>
2017-07-03 16:21:37 +0200

    set style for app landing page

--- src/main/java/be/mormont/iacf/boncom/ui/RootSceneController.java
+++ src/main/java/be/mormont/iacf/boncom/ui/RootSceneController.java
@@ -1,8 +1,12 @@
 package be.mormont.iacf.boncom.ui;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,9 +17,18 @@ import java.util.ResourceBundle;
  */
 public class RootSceneController implements Initializable {
     @FXML private Label titleLabel;
+    @FXML private VBox createOrderFormBox;
+    @FXML private VBox createProviderBox;
+    @FXML private Label createOrderFormLabel;
+    @FXML private Label createProviderLabel;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         titleLabel.setText("Bon de commandes");
+        createOrderFormLabel.setText("Nouveau bon de commande");
+        createProviderLabel.setText("Nouveau fournisseur");
+
+        createOrderFormBox.setOnMouseClicked(event -> System.out.println("Create new order form"));
+        createProviderBox.setOnMouseClicked(event -> System.out.println("Create new provider"));
     }
 }
