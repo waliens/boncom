6ce81a1bbac77673745f924012e6fe6ca20f2bdf
Romain Mormont <romainmormont@hotmail.com>
2017-07-08 22:52:01 +0200

    add number field + getter and setter

--- src/main/java/be/mormont/iacf/boncom/data/OrderForm.java
+++ src/main/java/be/mormont/iacf/boncom/data/OrderForm.java
@@ -1,15 +1,56 @@
 package be.mormont.iacf.boncom.data;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 
 /**
  * Created by Romain on 28-06-17.
  * This is a class.
  */
 public class OrderForm {
+    private long number;
     private Entity purchaser;
     private Entity provider;
-    private Date date;
+    private LocalDate date;
     private ArrayList<OrderFormEntry> entries;
+
+    public Entity getPurchaser() {
+        return purchaser;
+    }
+
+    public void setPurchaser(Entity purchaser) {
+        this.purchaser = purchaser;
+    }
+
+    public Entity getProvider() {
+        return provider;
+    }
+
+    public void setProvider(Entity provider) {
+        this.provider = provider;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public ArrayList<OrderFormEntry> getEntries() {
+        return entries;
+    }
+
+    public void setEntries(ArrayList<OrderFormEntry> entries) {
+        this.entries = entries;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public void setNumber(long number) {
+        this.number = number;
+    }
 }
